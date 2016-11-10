f7e3d44087a1eeb127af9b768bbbccbe1f72bd9f
Gareth Visagie <gareth@gjvis.com>
2016-11-10 09:06:39 +0000

    feat: ensure rubygem vulns have vulnerable versions

---
+++ scripts/ensure-rubygems-vulnerable-versions.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const rubySemver = require('@snyk/ruby-semver');
+const invert = require('@snyk/invert-semver-ranges')(rubySemver);
+
+const rubygemsPath = `${__dirname}/../data/rubygems`
+fs.readdirSync(rubygemsPath)
+.map(gemDirName => `${rubygemsPath}/${gemDirName}`)
+.forEach(gemDirPath => {
+  if (!fs.statSync(gemDirPath).isDirectory()) {
+    throw new Error(`'${gemDirPath}' is not a directory!`);
+  }
+  fs.readdirSync(gemDirPath)
+  .map(vulnDirName => `${gemDirPath}/${vulnDirName}`)
+  .forEach(vulnDirPath => {
+    const vulnDataPath = `${vulnDirPath}/data.json`;
+    const vulnData = require(vulnDataPath);
+    if (!vulnData.semver.vulnerable) {
+      const unaffected = vulnData.semver.unaffected || [];
+      const patched = vulnData.semver.patched || [];
+      vulnData.semver.vulnerable = invert(unaffected.concat(patched));
+      fs.writeFileSync(vulnDataPath,
+        JSON.stringify(vulnData, null, "\t") + "\n");
+    }
+  });
+});
